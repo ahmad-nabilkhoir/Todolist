e0138591712f9d8740bfcfcd15220892ee14f2b4
Ahmad Nabil Khoir <emailkamu@gmail.com>
2025-08-03 00:39:31 +0700

    Ganti nama Dashboard

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,4 @@ Route::middleware(['auth'])->group(function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
